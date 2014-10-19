e83239d49e791e45cdb1f74488b18afb06926f53
Aaron Dennis <aarondennis96@gmail.com>
2014-10-20 00:37:10 +0100

    Update THEtest.js

--- THEtest.js
+++ THEtest.js
@@ -1,12 +1,12 @@
-var theTest = prompt("Are you gay? (yes/no)")
+var theTest = prompt("Are you gay? (yes/no)");
 if (theTest === "yes") {
-  alert("I knew you were gay!")
+  alert("I knew you were gay!");
 }
 else if (theTest === "no"){
-  alert("Why are you lying to me? Try again")
+  alert("Why are you lying to me? Try again");
 }
 else {
-  alert("Please enter 'yes' or 'no'")
+  alert("Please enter either 'yes' or 'no'");
 }
 
 do {
