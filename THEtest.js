a8bfa8f6470ded1c1559aadeb8e39c3f83fce346
Aaron Dennis <aarondennis96@gmail.com>
2014-10-22 00:19:45 +0100

    Update THEtest.js

--- THEtest.js
+++ THEtest.js
@@ -1,5 +1,6 @@
 function myFunction() {
-var theTest = prompt("Are you gay? (yes/no)");
+  var name = prompt("What is your name?");
+var theTest = prompt(name + " are you gay? (yes/no)");
 while (theTest != "yes") {
 alert("Why are you lying to me? Try again.");
 var theTest = prompt("Are you gay? (yes/no)");
