bbca96d56a62048fae35aef8554feedd2feeab2d
Aaron Dennis <aarondennis96@gmail.com>
2014-11-13 18:45:58 +0000

    Update grootify.js

--- grootify.js
+++ grootify.js
@@ -1,1 +1,19 @@
-javascript:var words=['I am Groot. ','I AM GROOT! ','I am Groot. ','I am Groot. ','I am Groot! ','I am Groot. ','I am Groot. ','I AM GROOT! ','I am Groot? ','I... am Groot. ','I am Groot?! '];function groot(node){if(node.nodeType===Node.TEXT_NODE){var text=node.textContent;var iam=text.replace(/\b(\w\S*\s?){1,3}/ig,function(){var idx=Math.floor(Math.random()*words.length);return words[idx];});node.textContent=iam;return;}else if(node.nodeType===Node.ELEMENT_NODE){for(var i=0;i<node.childNodes.length;++i){groot(node.childNodes[i]);}return;}else{return;}}groot(document.body);
+javascript:var words=['I am Groot. ','I AM GROOT! ','I am Groot. ','I am Groot. ','I am Groot! ','I am Groot. ','I am Groot. ','I AM GROOT! ','I am Groot? ','I... am Groot. ','I am Groot?! '];
+function groot(node){
+  if(node.nodeType===Node.TEXT_NODE){
+    var text=node.textContent;
+    var iam=text.replace(/\b(\w\S*\s?){1,3}/ig,function(){var idx=Math.floor(Math.random()*words.length);
+    return words[idx];});
+    node.textContent=iam;return;
+    }
+  else if(node.nodeType===Node.ELEMENT_NODE){
+    for(var i=0;i<node.childNodes.length;++i){
+      groot(node.childNodes[i]);
+    }
+    return;
+  }
+  else{
+    return;
+  }
+}
+groot(document.body);
